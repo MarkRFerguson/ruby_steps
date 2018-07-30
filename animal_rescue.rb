674ecbc9fe88a1e38722915ca54ad5dce3f0de3c
Mark Ferguson <markferguson72@gmail.com>
2018-07-30 21:13:22 +0100

    first exercises with ruby

---
+++ animal_rescue.rb
@@ -0,0 +1,51 @@
+class Dog
+    def make_up_name
+        @name = "Sandy"
+    end
+
+    def talk
+        puts "#{@name} says Bark"
+    end
+
+    def move(destination)
+        puts "#{@name} runs to the #{destination}"
+    end
+
+    def make_up_age
+        @age = 5
+    end
+
+    def report_age
+        puts "#{@name} is #{@age} years old"
+    end
+end
+
+class Bird
+    def talk(name)
+        puts "#{name} says Chirp! Chirp!"
+    end
+
+    def move(name, destination)
+        puts "#{name} flies to the #{destination}"
+    end
+end
+
+class Cat
+    def talk(name)
+        puts "#{name} says Meoooow"
+    end
+
+    def move(name, destination)
+        puts "#{name} runs to the #{destination}"
+    end
+end
+
+
+dog = Dog.new
+dog.make_up_name
+dog.move("yard")
+dog.make_up_age
+dog.report_age
+
+
+
