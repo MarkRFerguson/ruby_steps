db36e57035b6e9105508ca3a91f2c8509db9b1e3
Mark Ferguson <41926322+MarkRFerguson@users.noreply.github.com>
2018-08-08 12:00:42 +0100

    Chapter 8 References

---
+++ astronomer.rb
@@ -0,0 +1,24 @@
+class CelestialBody
+    attr_accessor :type, :name
+end
+
+default_body = CelestialBody.new
+default_body.type = "planet"
+bodies = Hash.new do |hash, key|
+    body = CelestialBody.new
+    body.type = "planet"
+    hash[key] = body
+end
+
+bodies["Mars"].name = "Mars"
+
+bodies["Europa"].name = "Europa"
+bodies["Europa"].type = "moon"
+
+bodies["Venus"].name = "Venus"
+
+p bodies["Venus"]
+p bodies["Mars"]
+p bodies["Europa"]
+
+
