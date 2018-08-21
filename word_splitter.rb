7dd84d3f96bc2638632302f350f4c76c180c3b61
Mark Ferguson <41926322+MarkRFerguson@users.noreply.github.com>
2018-08-21 11:20:47 +0100

    Chapter 10 Comparable and Enumerable

---
+++ word_splitter.rb
@@ -0,0 +1,16 @@
+class WordSplitter
+
+    include Enumerable
+
+    attr_accessor :string
+
+    def each
+        string.split(" ").each do |word|
+            yield word
+        end
+    end
+
+end
+
+
+
