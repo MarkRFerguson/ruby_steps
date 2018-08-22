4aa6ddcfa86cb6164ebf22f708ed080d7ef97947
Mark Ferguson <41926322+MarkRFerguson@users.noreply.github.com>
2018-08-22 12:10:37 +0100

    Chapter 12 Exceptions

---
+++ code_magnets_ch12.rb
@@ -0,0 +1,18 @@
+def drive(destination)
+
+    if destination == "Hawaii"
+        raise "You can't drive to Hawaii"
+    end
+
+end
+
+begin
+
+    drive("Hawaii")
+
+    rescue => error
+        puts error.message
+
+    
+end
+
